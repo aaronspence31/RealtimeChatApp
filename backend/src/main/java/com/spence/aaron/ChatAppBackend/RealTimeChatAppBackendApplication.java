43c194c3ea75c5a0e107cf24759ac8789920bd7d
Aaron Spence <aaronspence31@gmail.com>
2023-09-03 13:18:59 -0400

    initial commit with backend structure

---
+++ backend/src/main/java/com/spence/aaron/ChatAppBackend/RealTimeChatAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spence.aaron.ChatAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeChatAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeChatAppBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/spence/aaron/ChatAppBackend/RealTimeChatAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spence.aaron.ChatAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeChatAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
